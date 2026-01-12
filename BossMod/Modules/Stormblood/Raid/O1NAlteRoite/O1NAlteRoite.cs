08852209762f677970d586c8ca8303f6975ecf99
Adzcriz <90148778+Adzcriz@users.noreply.github.com>
2026-01-12 19:12:24 +0000

    just some updates

--- BossMod/Modules/Stormblood/Raid/O1NAlteRoite/O1NAlteRoite.cs
+++ BossMod/Modules/Stormblood/Raid/O1NAlteRoite/O1NAlteRoite.cs
@@ -1,48 +1,39 @@
+using BossMod.Components;
+
 namespace BossMod.Stormblood.Raid.O1NAlteRoite;
 
+// Working
 sealed class Roar(BossModule module) : Components.RaidwideCast(module, (uint)AID.Roar);
 sealed class ThinIce(BossModule module) : Components.ThinIce(module, 6f, true, (uint)SID.ThinIce, true);
-sealed class Burn(BossModule module) : Components.SimpleAOEs(module, (uint)AID.Burn, new AOEShapeCircle(8f));
-sealed class Levinbolt1(BossModule module) : Components.SimpleAOEs(module, (uint)AID.Levinbolt1, new AOEShapeCircle(6f));
-[SkipLocalsInit]
-sealed class BreathWing(BossModule module) : Components.SimpleKnockbacks(module, (uint)AID.BreathWing, 10f, kind: Kind.DirForward, stopAtWall: true);
-sealed class WyrmTail(BossModule module) : Components.SingleTargetCast(module, (uint)AID.WyrmTail);
-sealed class TwinBolt(BossModule module) : Components.SingleTargetCast(module, (uint)AID.TwinBolt);
-sealed class Blaze(BossModule module) : Components.StackTogether(module, (uint)AID.Blaze, activationDelay: 5.0f, radius: 6f);
-[SkipLocalsInit]
-sealed class Levinbolt(BossModule module) : Components.SpreadFromCastTargets(module, (uint)AID.Levinbolt, 5f)
-{
-    public override void AddHints(int slot, Actor actor, TextHints hints)
-    {
-        if (IsSpreadTarget(actor))
-        {
-            hints.Add("Spread!");
-        }
-    }
-}
-[SkipLocalsInit]
-
-sealed class Clamp(BossModule module) : Components.SimpleKnockbacks(module, (uint)AID.Clamp, 10f, shape: new AOEShapeRect(10f, 9f), kind: Kind.DirForward, stopAtWall: true);
-   
-sealed class Flame(BossModule module) : Components.RaidwideCast(module, (uint)AID.Flame);
 sealed class Charybdis(BossModule module) : Components.RaidwideCast(module, (uint)AID.Charybdis);
-   
-   
+sealed class Burn(BossModule module) : Components.SimpleAOEs(
+    module,
+    (uint)AID.Burn1,
+    new AOEShapeCircle(8f));
+// Twinbolt Stuff
+sealed class TwinBoltTetheredBuster(BossModule module)
+    : Components.BaitAwayTethers(
+        module,
+        shape: new AOEShapeCircle(6),          // TODO radius
+        tetherID: (uint)TetherID.TwinBolt,     // TODO correct tether id
+        aid: (uint)AID.TwinBolt1                // TODO correct action id
+      )
+{ }
+// Fire Orbs Burn
+sealed class FireOrbBurnFast(BossModule module)
+    : Components.SimpleAOEs(module, (uint)AID.Burn, new AOEShapeCircle(8f));
 
-
-
+sealed class FireOrbBurnSlow(BossModule module)
+    : Components.SimpleAOEs(module, (uint)AID.Burn1, new AOEShapeCircle(8f));
 
 [ModuleInfo(BossModuleInfo.Maturity.WIP,
 ObjectIDType = typeof(OID),
 ActionIDType = typeof(AID), // replace null with typeof(AID) if applicable
 StatusIDType = typeof(SID), // replace null with typeof(SID) if applicable
 TetherIDType = typeof(TetherID), // replace null with typeof(TetherID) if applicable
-IconIDType = null, // replace null with typeof(IconID) if applicable
+IconIDType = typeof(IconID), // replace null with typeof(IconID) if applicable
 Contributors = "JoeSparkx",
 GroupType = BossModuleInfo.GroupType.CFC,
 GroupID = 252,
 NameID = 5629)]
 public class O1NAlteRoite(WorldState ws, Actor primary) : BossModule(ws, primary, new(00, 00), new ArenaBoundsCircle(20));
-
-
-
--- BossMod/Modules/Stormblood/Raid/O1NAlteRoite/O1NAlteRoiteStates.cs
+++ BossMod/Modules/Stormblood/Raid/O1NAlteRoite/O1NAlteRoiteStates.cs
@@ -1,20 +1,17 @@
 namespace BossMod.Stormblood.Raid.O1NAlteRoite;
+
 sealed class O1NAlteRoiteStates : StateMachineBuilder
 {
     public O1NAlteRoiteStates(BossModule module) : base(module)
     {
         TrivialPhase()
-            .ActivateOnEnter<Burn>()
             .ActivateOnEnter<Roar>()
-            .ActivateOnEnter<Blaze>()
-            .ActivateOnEnter<BreathWing>()
             .ActivateOnEnter<ThinIce>()
-            .ActivateOnEnter<WyrmTail>()
-            .ActivateOnEnter<TwinBolt>()
-            .ActivateOnEnter<Levinbolt>()
-            .ActivateOnEnter<Clamp>()
-            .ActivateOnEnter<Flame>()
             .ActivateOnEnter<Charybdis>()
-            .ActivateOnEnter<Levinbolt1>();
+            .ActivateOnEnter<TwinBoltTetheredBuster>()
+            .ActivateOnEnter<Burn>()
+            .ActivateOnEnter<FireOrbBurnFast>()
+            .ActivateOnEnter<FireOrbBurnSlow>()
+;
     }
 }
